109ba9ac0bec39f6a1a00b400e2a3e94f1be82e0
louweal <anneloeslouwe@live.nl>
2025-08-01 13:27:45 +0200

    debugging

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,7 +42,7 @@ export default defineNuxtConfig({
     postcss: {
         plugins: {
             "postcss-nested": {},
-            "@tailwindcss/postcss": {},
+            // "@tailwindcss/postcss": {},
             autoprefixer: {},
         },
     },
